14b48929944fb7de64b717c5a9fc55c48fa1eb54
Michael Aubrey <michael@akita-itlabo.com>
2019-12-03 18:07:05 +0900

    make tests use ts version, add badges/coverage

--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,9 @@
-const Timer = require("./index.js");
+const TestTimer = require('./index')
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 test(`returns a 500 ms delay as '500 miliseconds'.`, async () => {
-  const timer = new Timer();
+  const timer = new TestTimer();
   await delay(500);
   const elapsed = timer.elapsed();
   const number = parseInt(elapsed.split(" ")[0]);
@@ -14,19 +14,56 @@ test(`returns a 500 ms delay as '500 miliseconds'.`, async () => {
 });
 
 test(`can convert 60000 miliseconds into '1 minute'.`, () => {
-  const timer = new Timer();
+  const timer = new TestTimer();
   const elapsed = timer.getReadable(60000);
   expect(elapsed).toBe("1 minute");
 });
 
+test(`can use elapsedBrief to convert 60000 miliseconds into '1m'.`, () => {
+  const timer = new TestTimer();
+  const now = new Date().getTime();
+  const elapsed = timer.elapsedBrief({
+    start: now - 60000,
+    end: now
+  });
+  expect(elapsed).toBe("1m");
+});
+
+test(`can use elapsedVerbose to convert 60000 miliseconds into '1 minute'.`, () => {
+  const timer = new TestTimer();
+  const now = new Date().getTime();
+  const elapsed = timer.elapsedVerbose({
+    start: now - 60000,
+    end: now
+  });
+  expect(elapsed).toBe("1 minute");
+});
+
+test(`can set brief to true on reset`, () => {
+  const timer = new TestTimer();
+  timer.reset({
+    brief: true
+  });
+  expect(timer.brief).toBe(true);
+});
+
+test(`can set start time on reset`, () => {
+  const thePast = new Date("September 2, 1885").getTime();
+  const timer = new TestTimer();
+  timer.reset({
+    start: thePast
+  });
+  expect(timer.start).toBe(thePast);
+});
+
 test(`can convert 3600000 miliseconds into '1 hour'.`, () => {
-  const timer = new Timer();
+  const timer = new TestTimer();
   const elapsed = timer.getReadable(3600000);
   expect(elapsed).toBe("1 hour");
 });
 
 test(`can init with options to show brief readable time`, () => {
-  const timer = new Timer({
+  const timer = new TestTimer({
     brief: true
   });
   const elapsed = timer.getReadable(5000);
@@ -34,7 +71,7 @@ test(`can init with options to show brief readable time`, () => {
 });
 
 test(`can output elapsed raw as over 9000 (笑)`, () => {
-  const timer = new Timer({
+  const timer = new TestTimer({
     start: new Date().getTime() - 10000
   });
   const elapsed = timer.elapsedRaw();
@@ -43,7 +80,7 @@ test(`can output elapsed raw as over 9000 (笑)`, () => {
 
 test(`can compare arbitrary times by using options`, () => {
   const now = new Date().getTime();
-  const timer = new Timer({
+  const timer = new TestTimer({
     start: now - 10000
   });
   const elapsed = timer.elapsedRaw({ end: now + 10000 });
@@ -51,7 +88,7 @@ test(`can compare arbitrary times by using options`, () => {
 });
 
 test(`can compare arbitrary times by setting start and end in elapsedRaw only`, () => {
-  const timer = new Timer({
+  const timer = new TestTimer({
     brief: true
   });
   const now = new Date().getTime();
@@ -59,5 +96,5 @@ test(`can compare arbitrary times by setting start and end in elapsedRaw only`, 
     start: now - 10000,
     end: now + 10000
   });
-  expect(elapsed).toBe('20s');
+  expect(elapsed).toBe("20s");
 });
